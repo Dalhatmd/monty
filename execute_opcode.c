06c0d5ec984073f1a1b88261c9a2e0850dafbd37
Dalhatmd <umarwalid4055@gmail.com>
2024-01-18 12:16:21 +0100

    add push function

--- execute_opcode.c
+++ execute_opcode.c
@@ -10,10 +10,11 @@ int execute(char *line, stack_t **stack, unsigned int count, FILE *file)
 	unsigned int i = 0;
 	char *op_code;
 
-	op_code = strtok(line, "\t\n");
+	op_code = strtok(line, " \t\n");
+/*	printf("%s\n", op_code);*/
 	if (op_code && op_code[0] == '#')
 		return (0);
-	data.arg = strtok(NULL, "\t\n");
+	data.arg = strtok(NULL, " \t\n");
 	while (instructions[i].opcode && op_code)
 	{
 		if (strcmp(op_code, instructions[i].opcode) == 0)
@@ -27,7 +28,6 @@ int execute(char *line, stack_t **stack, unsigned int count, FILE *file)
 	{
 		fprintf(stderr, "L%d unknown instruction %s\n", count, op_code);
 		fclose(file);
-		free(line);
 		exit(EXIT_FAILURE);
 	}
 	return (1);
--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main(int argc, char **argv)
 	FILE *fd;
 	char *line = NULL;
 	size_t len = 0;
-	ssize_t bytes_read;
+	ssize_t bytes_read = 1;
 	unsigned int line_no = 0;
 
 	if (argc != 2)
@@ -27,13 +27,14 @@ int main(int argc, char **argv)
 	while (bytes_read != -1)
 	{
 		bytes_read = getline(&line, &len, fd);
+/*		fwrite(line, bytes_read, 1, stdout);*/
 		data.line = line;
 		line_no++;
 		if (bytes_read != -1)
 		{
 			execute(line, &stack, line_no, fd);
 		}
-		free(line);
+/*		free(line);*/
 	}
 	fclose(fd);
 	return (0);
