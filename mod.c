b8ae5fe0c303d5543be52d96496b0a5f1fa7b4cb
Dalhatmd <umarwalid4055@gmail.com>
2024-01-20 17:40:18 +0100

    Add mod function

---
+++ mod.c
@@ -0,0 +1,34 @@
+#include "monty.h"
+/**
+ * mod - computes the modulus of the top and second of the stack
+ *
+ * @stack: input stack
+ * @count: line number
+ */
+void mod(stack_t **stack, unsigned int count)
+{
+	stack_t *head;
+	int mod;
+
+	head = *stack;
+	if(count_elem(head) < 2)
+	{
+		fprintf(stderr, " L%u: can't mod, stack too short\n", count);
+		fclose(data.file);
+		free(data.line);
+		_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+	if (head->n == 0 || head->next->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", count);
+		fclose(data.file);
+		free(data.line);
+		_free(*stack);
+		exit(EXIT_FAILURE);
+	}
+	mod = head->next->n % head->n;
+	head->next->n = mod;
+	head->next->prev = NULL;
+	*stack = head->next;
+}
